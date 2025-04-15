deafc952241cbdc6f84e0723de4c0c03a3edf022
Diego Crespo <147269640+D1nx0x@users.noreply.github.com>
2025-04-15 20:18:26 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-import "bootstrap";
-import "./style.css";
+document.addEventListener("DOMContentLoaded", Card); // Cuando el DOM esté listo, ejecuta este código
 
+function Card() {
+  const simbol = ["♦", "♥", "♠", "♣"];
+  const number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  // Seleccionamos aleatoriamente un simbolo o un numero //
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let randomSimbol = simbol[Math.floor(Math.random() * simbol.length)];
+  let randomNumber = number[Math.floor(Math.random() * number.length)];
+
+  // Agrega los valores de los numeros y simbolos //
+
+  document.querySelector("#simbol-left").textContent = randomSimbol;
+  document.querySelector("#simbol-right").textContent = randomSimbol;
+  document.querySelector("#number-center").textContent = randomNumber;
+
+  // Cambia el color de los simbolos //
+
+  if (randomSimbol === "♥" || randomSimbol === "♦") {
+    document
+      .querySelectorAll("#simbol-left, #simbol-right")
+      .forEach((rojo) => (rojo.style.color = "red"));
+  } else {
+    document
+      .querySelectorAll("#simbol-left, #simbol-right")
+      .forEach((negro) => (negro.style.color = "black"));
+  }
+}
+
